208f52c02a24c1c5f43aa21ff63292e13159c369
Mel <angel.melendez.c@gmail.com>
2024-03-13 17:29:49 -0600

    connect prisma and rainbow to siwe implementation

--- packages/nextjs/lib/auth/index.ts
+++ packages/nextjs/lib/auth/index.ts
@@ -1,18 +1,18 @@
-// import { User } from "@prisma/client";
+import { User } from "@prisma/client";
 import { NextAuthOptions } from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
-import { RandomWordOptions } from "random-word-slugs";
+import { getCsrfToken } from "next-auth/react";
+import { RandomWordOptions, generateSlug } from "random-word-slugs";
 import { SiweMessage } from "siwe";
-
-// import prisma from "../../../services/prisma";
+import prisma from "~~/services/prisma";
 
 const usernameSlugOptions: RandomWordOptions<3> = {
-    format: "camel",
-    partsOfSpeech: ["adjective", "noun", "adjective"],
-    categories: {
-        adjective: ["color", "appearance"],
-        noun: ["animals"],
-    },
+  format: "camel",
+  partsOfSpeech: ["adjective", "noun", "adjective"],
+  categories: {
+    adjective: ["color", "appearance"],
+    noun: ["animals"],
+  },
 };
 
 export const authOptions: NextAuthOptions = {
@@ -38,16 +38,38 @@ export const authOptions: NextAuthOptions = {
           placeholder: "0x0",
         },
       },
-      async authorize(credentials:any , req:any ) {
-        // let user: User;
+      async authorize(credentials, req) {
+        let user: User;
         try {
           const siwe = new SiweMessage(JSON.parse(credentials?.message || "{}"));
           const nextAuthUrl = new URL(process.env.NEXTAUTH_URL ?? "");
 
+          const result = await siwe.verify({
+            signature: credentials?.signature || "",
+            domain: nextAuthUrl.host,
+            nonce: await getCsrfToken({ req: { headers: req.headers } }),
+          });
+
+          if (result.success) {
+            const existingUser = await prisma.user.findFirst({
+              where: { id: siwe.address },
+            });
+
+            if (!existingUser) {
+              user = await prisma.user.create({
+                data: {
+                  id: siwe.address,
+                  username: generateSlug(3, usernameSlugOptions),
+                },
+              });
+            } else {
+              user = existingUser;
+            }
             return {
-                // id: user.id,
-                id: '0',
+              id: user.id,
             };
+          }
+          return null;
         } catch (e) {
           return null;
         }
